8fd81a37a22f3706c857502f38fd1138c023d6c0
Rajveer028 <147462246+Rajveer028@users.noreply.github.com>
2025-10-06 17:20:02 +0530

    final7

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,8 +1,5 @@
 import { StackHandler } from "@stackframe/stack";
-import type { ComponentProps } from "react";
 
-type HandlerProps = ComponentProps<typeof StackHandler>;
-
-export default function Handler(props: HandlerProps) {
-  return <StackHandler {...props} />;
+export default function Handler() {
+  return <StackHandler />;
 }
